01abeb82d56c9de9ae7009f0919bfe68e0e21490
San Kim <waterfog9580@gmail.com>
2021-08-17 18:50:09 +0900

    update

--- back-end/user_manager/src/main/java/waterfogsw/user_manager/repository/MemoryUserRepository.java
+++ back-end/user_manager/src/main/java/waterfogsw/user_manager/repository/MemoryUserRepository.java
@@ -33,4 +33,8 @@ public class MemoryUserRepository implements UserRepository {
     public List<User> findAll() {
         return new ArrayList<>(store.values());
     }
+
+    public void clearStore() {
+        store.clear();
+    }
 }
--- back-end/user_manager/src/test/java/waterfogsw/user_manager/MemoryUserRepositoryTest.java
+++ back-end/user_manager/src/test/java/waterfogsw/user_manager/MemoryUserRepositoryTest.java
@@ -1,5 +1,54 @@
 package waterfogsw.user_manager;
 
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.List;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import waterfogsw.user_manager.domain.User;
+import waterfogsw.user_manager.repository.MemoryUserRepository;
+
 public class MemoryUserRepositoryTest {
-    
+    MemoryUserRepository repository = new MemoryUserRepository();
+
+    @AfterEach
+    public void afterEach() {
+        repository.clearStore();
+    }
+
+    @Test
+    public void save() {
+        User user = new User();
+        user.setName("san kim");
+
+        repository.save(user);
+
+        User result = repository.findById(user.getId()).get();
+        assertThat(result).isEqualTo(user);
+    }
+
+    @Test
+    public void findByName() {
+        User user = new User();
+        user.setName("san kim");
+
+        repository.save(user);
+        
+        User result = repository.findByName("san kim").get();
+        assertThat(result).isEqualTo(user);
+    }
+
+    @Test
+    public void findAll() {
+        User user1 = new User();
+        User user2 = new User();
+
+        repository.save(user1);
+        repository.save(user2);
+
+        List<User> arrayList = repository.findAll();
+        assertThat(arrayList.size()).isEqualTo(2);
+    }
 }
