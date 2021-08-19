a279d5be55c53376617c5a78674069a30809516b
San Kim <waterfog9580@gmail.com>
2021-08-19 12:09:26 +0900

    update

--- back-end/user_manager/src/main/java/waterfogsw/user_manager/controller/HomeController.java
+++ back-end/user_manager/src/main/java/waterfogsw/user_manager/controller/HomeController.java
@@ -1,13 +1,28 @@
 package waterfogsw.user_manager.controller;
 
+import java.util.List;
+
+import org.springframework.ui.Model;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import waterfogsw.user_manager.domain.User;
+import waterfogsw.user_manager.service.UserService;
+
 @Controller
 public class HomeController {
+    private final UserService userService;
     
+    @Autowired
+    public HomeController(UserService userservice) {
+        this.userService = userservice;
+    }
+
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+        List<User> users = userService.findUsers();
+        model.addAttribute("users", users);
         return "home";
     }
 }
